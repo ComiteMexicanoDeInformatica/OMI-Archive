ed2b21cb9f9e9b4343991a830dbf4271ce583fab
Diego García <diegomysterio@gmail.com>
2020-10-15 11:38:30 -0500

    format logias

---
+++ 2017/OMI-2017-Logias/tests/test-validator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import logging
+import unittest
+import sys
+import re
+
+from omegaup.validator import validatortest
+
+
+class MyTest(unittest.TestCase):
+    def test_input(self):
+        original_input = open('data.in', 'r').read()
+
+        regex = re.compile(r'^(\d+) (\d+) (\d+)\n$')
+        results = re.match(regex, original_input).groups()
+        self.assertEqual(len(results), 2)
+
+        N = int(results[0])
+        M = int(results[1])
+
+        self.assertTrue(0 <= N <= 1e9)
+        self.assertTrue(0 <= M <= 1e9)
+
+if __name__ == '__main__':
+    validatortest.main()
