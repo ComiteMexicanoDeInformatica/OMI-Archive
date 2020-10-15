23bbee27c0b4af717bf2ffb42e02f7a844dd18d9
Diego García <diegomysterio@gmail.com>
2020-10-15 17:46:31 -0500

    fix indentation

--- 2017/OMI-2017-Logias/tests/test-validator.py
+++ 2017/OMI-2017-Logias/tests/test-validator.py
@@ -10,18 +10,17 @@ from omegaup.validator import validatortest
 
 class MyTest(unittest.TestCase):
     def test_input(self):
-        with open('data.in', 'r') as f:
-	    	original_input = f.read()
+    	with open('data.in', 'r') as f:
+    		original_input = f.read()
+	        regex = re.compile(r'^(\d+) (\d+)\n$')
+	        results = re.match(regex, original_input).groups()
+	        self.assertEqual(len(results), 2)
 
-        regex = re.compile(r'^(\d+) (\d+)\n$')
-        results = re.match(regex, original_input).groups()
-        self.assertEqual(len(results), 2)
+	        N = int(results[0])
+	        M = int(results[1])
 
-        N = int(results[0])
-        M = int(results[1])
-
-        self.assertTrue(0 <= N <= 1e9)
-        self.assertTrue(0 <= M <= 1e9)
+	        self.assertTrue(0 <= N <= 1e9)
+	        self.assertTrue(0 <= M <= 1e9)
 
 if __name__ == '__main__':
     validatortest.main()
