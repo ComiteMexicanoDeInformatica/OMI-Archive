e71fbd3ec52c2f68e40972c88595d285dfc55460
Javier Pantoja <javierjpc1305@gmail.com>
2020-10-19 09:33:21 -0500

    format Mapa

--- 2018/OMI-2018-Mapa/tests/test-validator.py
+++ 2018/OMI-2018-Mapa/tests/test-validator.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import logging
+import unittest
 import sys
-import os
 import re
 
-from ctest import *
+from omegaup.validator import validatortest
 
-class Test(CTest):
-    def test(self):
-        regex = re.compile(r'^(\d+) (\d+) (\d+)\n((?:[01]+\n)+)$')
-        results = re.match(regex, self.input).groups()
-        self.assertEqual(len(results), 4)
+class MyTest(unittest.TestCase):
+	def test(self):
+		with open('data.in', 'r') as f:
+			original_input = f.read()
 
-        N = int(results[0])
-        M = int(results[1])
-        K = int(results[2])
-        
-        grid = results[3].split('\n')
-        grid.pop()
+		regex = re.compile(r'^(\d+) (\d+) (\d+)\n((?:[01]+\n)+)$')
+		results = re.match(regex, original_input).groups()
+		self.assertEqual(len(results), 4)
 
-        self.assertTrue(1 <= N <= 1500)
-        self.assertTrue(1 <= M <= 1500)
-        self.assertTrue(0 <= K <= 6)
+		N = int(results[0])
+		M = int(results[1])
+		K = int(results[2])
+		
+		grid = results[3].split('\n')
+		grid.pop()
 
-        self.assertEqual(len(grid), N)
-        self.assertTrue(all(len(r) == M for r in grid))
+		self.assertTrue(1 <= N <= 1500)
+		self.assertTrue(1 <= M <= 1500)
+		self.assertTrue(0 <= K <= 6)
 
-        if 'easy' in self.caseName:
-            self.assertTrue(1 <= N <= 20)
-            self.assertTrue(1 <= M <= 20)
+		self.assertEqual(len(grid), N)
+		self.assertTrue(all(len(r) == M for r in grid))
 
-Test().run()
+		caseName = sys.argv[1]
+		if 'easy' in caseName:
+			self.assertTrue(1 <= N <= 20)
+			self.assertTrue(1 <= M <= 20)
+
+if __name__ == '__main__':
+	validatortest.main()
