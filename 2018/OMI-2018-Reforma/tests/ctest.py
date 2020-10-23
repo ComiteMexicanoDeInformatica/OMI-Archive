75ea10e0420ac85a3cfced4d3ba1ffee7752d9fd
Janque <69932806+Janque@users.noreply.github.com>
2020-10-23 13:16:48 -0500

    format reforma (#34)
    * format reforma
    
    * Apply suggestions from code review
    
    Co-authored-by: Freddy Román <me@freddy.mx>
    
    Co-authored-by: Freddy Román <me@freddy.mx>

--- 2018/OMI-2018-Reforma/tests/ctest.py
+++
@@ -1,1 +0,0 @@
-../../../utils/ctest.py
--- 2018/OMI-2018-Reforma/tests/test-validator.py
+++ 2018/OMI-2018-Reforma/tests/test-validator.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import logging
+import unittest
 import sys
-import os
 import re
 
-from ctest import *
+from omegaup.validator import validatortest
 
-class Test(CTest):
-    def test(self):
-        regex = re.compile(r'(\d*)\n(\d*(?: \d*)*)\n$')
-        results = re.match(regex, self.input).groups()
-        self.assertEqual(len(results), 2)
+class MyTest(unittest.TestCase):
+	def test(self):
+		with open('data.in', 'r') as f:
+			original_input = f.read()
+		
+		regex = re.compile(r'(\d*)\n(\d*(?: \d*)*)\n$')
+		results = re.match(regex, original_input).groups()
+		self.assertEqual(len(results), 2)
 
-        N = int(results[0])
-        nums = [int(x) for x in results[1].split()]
+		N = int(results[0])
+		nums = [int(x) for x in results[1].split()]
 
-        self.assertEqual(N, len(nums))
-        self.assertTrue(0 <= min(nums))
-        self.assertTrue(max(nums) <= 2**16)
-        
-        if 'casogrande' in self.caseName:
-            self.assertTrue(0 <= N <= 10**5)
-        else:
-            self.assertTrue(0 <= N <= 1000)
+		self.assertEqual(N, len(nums))
+		self.assertTrue(0 <= min(nums))
+		self.assertTrue(max(nums) <= 2**16)
+		
+		caseName = sys.argv[1]
+		if 'casogrande' in caseName:
+			self.assertTrue(0 <= N <= 10**5)
+		else:
+			self.assertTrue(0 <= N <= 1000)
 
-Test().run()
+if __name__ == '__main__':
+	validatortest.main()
